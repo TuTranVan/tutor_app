7e17459f04bb0c18f2296ed9c2da30ff41e036a2
Tu <tranvantu.dev@gmail.com>
2019-04-09 14:06:15 +0700

    Certificates of tutor

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,7 +1,7 @@
 class Certificate < ApplicationRecord
   belongs_to :tutor
 
-  validates :shool, presence: true
+  validates :school, presence: true
   validates :major, presence: true
   validates :grade, presence: true
   validates :course, presence: true
